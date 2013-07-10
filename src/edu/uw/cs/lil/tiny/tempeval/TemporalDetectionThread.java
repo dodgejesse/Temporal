651dca8da56111fe108226fe452b2f6588ba42dc
Kenton Lee <kentonl@seas.upenn.edu>
2013-07-09 18:27:05 -0700

    Some fixes to detection

--- src/edu/uw/cs/lil/tiny/tempeval/TemporalDetectionThread.java
+++ src/edu/uw/cs/lil/tiny/tempeval/TemporalDetectionThread.java
@@ -29,7 +29,7 @@ public class TemporalDetectionThread extends Thread {
 	final JointModel<Sentence, String[], LogicalExpression, LogicalExpression> model;
 	final TemporalSentenceDataset train, test;
 	final AbstractCKYParser<LogicalExpression> parser;
-	
+
 	public TemporalDetectionThread(TemporalSentenceDataset train,
 			TemporalSentenceDataset test,
 			AbstractCKYParser<LogicalExpression> parser,
@@ -68,12 +68,12 @@ public class TemporalDetectionThread extends Thread {
 		}
 		return newAllModelParses;
 	}
-	
+
 	private boolean hasOverlap (int index1, String s1, int index2, String s2) {
 		// Whether the first and second string at respective indexes have overlapping regions
 		return (index1 + s1.length() - 1) >= index2 && index1 <= (index2 + s2.length() - 1);
 	}
-	
+
 	private boolean hasCorrectMention(List<Pair<Integer, String>> predictedMentions, int goldCharNum, String goldMention) {
 		for (Pair<Integer, String> p : predictedMentions) {
 			if(hasOverlap(p.first(), p.second(), goldCharNum, goldMention.toString())) {
@@ -82,12 +82,15 @@ public class TemporalDetectionThread extends Thread {
 		}
 		return false;
 	}
-	
+
 	private List<Pair<Integer, String>> getPredictedMentions(String fullSentence, JointDataItemModel<Sentence, String[], LogicalExpression, LogicalExpression> jointDataItemModel) {
 		List<String> tokens = new Sentence(fullSentence).getTokens();
-
+		tokens = new ArrayList<String>(tokens);
 		List<Pair<Integer, String>> predictedMentions = new LinkedList<Pair<Integer, String>>();
-		for (int i = 0 ; i < tokens.size(); i++) {					
+		for (int i = 0 ; i < tokens.size(); i++) {	
+			String t = tokens.get(i);
+			if (t.endsWith("'s"))
+				tokens.set(i, t.substring(0, t.length() - 2));
 			// Work backwards to get the longest possible mention
 			for (int span = Math.min(MAX_MENTION_LENGTH, tokens.size() - i)  ; span > 0 ; span--) {
 				Sentence mentionCandidate = new Sentence(tokens.subList(i, i + span));
@@ -95,9 +98,7 @@ public class TemporalDetectionThread extends Thread {
 				final List<IParseResult<LogicalExpression>> isoParses = pruneLogicWithLambdas(parserOutput.getAllParses());
 				if (isoParses.size() > 0) {
 					int predictedCharNum = fullSentence.toString().indexOf(mentionCandidate.toString());
-					predictedMentions.add(Pair.of(predictedCharNum, mentionCandidate.toString()));
-					System.out.printf ("Predicted mention [#%d]: '%s' (%s)\n", predictedCharNum, mentionCandidate, isoParses.get(0));
-					
+					predictedMentions.add(Pair.of(predictedCharNum, mentionCandidate.toString()));					
 					// skip used tokens
 					i += span - 1; // - 1 to account for i++ in for-loop
 					break;
@@ -106,44 +107,45 @@ public class TemporalDetectionThread extends Thread {
 		}	
 		return predictedMentions;
 	}
-	
+
 	public void run(){
-		System.out.println("About to parse all substrings");
-		
 		Map<String, List<Pair<Integer, String>>> predictedMentionsMap = new HashMap<String, List<Pair<Integer, String>>>();
 		int numPredictedMentions = 0;
 		int numGoldMentions = 0;
 		int numCorrectMentions = 0;
-		
+
 		for (final ILabeledDataItem<Pair<Sentence, String[]>, TemporalResult> dataItem : train) {
 			// Retrieve relevant fields
-			String fullSentence = dataItem.getSample().second()[1].replace("-", " ").replace(","," ").replace("."," ").toLowerCase();
+			String fullSentence = dataItem.getSample().second()[1].replace("-", " ").replace(",","").toLowerCase();
+			if (fullSentence.charAt(fullSentence.length() - 1) == '.')
+				fullSentence = fullSentence.substring(0, fullSentence.length() - 1);
 			String goldMention = dataItem.getSample().first().toString();
 			int goldCharNum = Integer.parseInt(dataItem.getSample().second()[4]);
 			JointDataItemModel<Sentence, String[], LogicalExpression, LogicalExpression> jointDataItemModel = 
 					new JointDataItemModel<Sentence, String[], LogicalExpression, LogicalExpression>(model, dataItem);
-			
+
 			numGoldMentions++;
-			
-			// Per-sentence info
-			System.out.printf ("Sentence: '%s'\n", fullSentence);
-			System.out.printf ("Gold mention [#%d]: '%s'\n", goldCharNum, goldMention);
-			
+
 			// Predict the mentions if they weren't already predicted
 			if(!predictedMentionsMap.containsKey(fullSentence)) {
 				List<Pair<Integer, String>> predictedMentions = getPredictedMentions(fullSentence, jointDataItemModel);
 				predictedMentionsMap.put(fullSentence, predictedMentions);
 				numPredictedMentions += predictedMentions.size();
 			}
-			
+
 			if (hasCorrectMention(predictedMentionsMap.get(fullSentence), goldCharNum, goldMention.toString())) {
-				System.out.println("Correct mention detected");
+				//System.out.println("Correct mention detected");
 				numCorrectMentions++;
 			}
 			else {
 				System.out.println("No correct mention detected");
+				// Per-sentence info
+				System.out.printf ("Sentence: '%s'\n", fullSentence);
+				System.out.printf ("Gold mention [#%d]: '%s'\n", goldCharNum, goldMention);
+				for (Pair<Integer, String> p : predictedMentionsMap.get(fullSentence))
+					System.out.printf ("Predicted mention [#%d]: '%s'\n", p.first(), p.second());
+				System.out.println();
 			}
-			System.out.println();
 		}
 		System.out.println("\nMention detection stats:");
 		System.out.printf("Recall: %f\n", getRecall(numCorrectMentions, numGoldMentions, numPredictedMentions));
@@ -158,7 +160,7 @@ public class TemporalDetectionThread extends Thread {
 	private double getPrecision(int correct, int gold, int predicted) {
 		return ((double) correct)/predicted;
 	}
-	
+
 	private double getF1(int correct, int gold, int predicted) {
 		double r = getRecall(correct, gold, predicted);
 		double p = getPrecision(correct, gold, predicted);
